d0b9dceab58732bb321379856cccfa471b87999d
ShravaniLatthe <lattheshravani8.in>
2026-06-11 14:19:37 +0530

    Added Task 1 Ecommerce Website

---
+++ ecommerce/store/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products')
+
+    def __str__(self):
+        return self.name
+    
+from django.contrib.auth.models import User
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    @property
+    def subtotal(self):
+        return self.product.price * self.quantity
+    def __str__(self):
+        return self.user.username
+    
+class Order(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    total_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.user.username
